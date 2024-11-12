4210de38fff4fdb5ab82b5c94ea96fe3e8d47ae6
Kaushal <kaushalkr8@gmail.com>
2024-11-12 17:55:39 +0530

    updated feature

--- src/pages/AllUser.jsx
+++ src/pages/AllUser.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { addFollow, deleteFollow } from "../features/userSlice";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const AllUser = ({ allUser, userDetails }) => {
   const dispatch = useDispatch();
